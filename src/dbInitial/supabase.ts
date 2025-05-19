212c9769d794974c14a63f08fb4bfb02ab9f7915
raffimahya <raffimlbb.23@gmail.com>
2025-05-20 00:11:13 +0700

    Update Supabase initialization

--- src/dbInitial/supabase.ts
+++ src/dbInitial/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://thufxdqmownucaydlkeg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRodWZ4ZHFtb3dudWNheWRsa2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NzcxMjMsImV4cCI6MjA2MzE1MzEyM30.eq0VuBK8StAy2RbTs1Rg48aZpbdQvlR_0ddSrciWClE')
+export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
